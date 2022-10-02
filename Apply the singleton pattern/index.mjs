db2256aa1c19ccc13ff5871b1542cf372f7fa4c8
Martib01 <martybb01@gmail.com>
2022-10-02 10:21:29 +0200

    End es singleton pattern

---
+++ Apply the singleton pattern/index.mjs
@@ -0,0 +1,4 @@
+import logger from "./script.mjs";
+
+logger.output("ciao");
+logger.output("come stai?");
---
+++ Apply the singleton pattern/script.mjs
@@ -0,0 +1,8 @@
+class Logger {
+  output(value) {
+    console.log(value);
+  }
+}
+
+const logger = new Logger();
+export default logger;
